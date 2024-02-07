de807d1941ea13bd8458d9b3b41db7460ac92047
Tsundere808 <xhawaii808@gmail.com>
2024-02-07 13:59:47 -1000

    *added example to find distance https://docs.limelightvision.io/docs/docs-limelight/tutorials/tutorial-estimating-distance

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -151,7 +151,31 @@ public class PivotSubsystem extends SubsystemBase {
   }
 
   public void adjustPidShoot(){
-   
+      
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry ty = table.getEntry("ty");
+
+    double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+    // how many degrees back is your limelight rotated from perfectly vertical?
+    double limelightMountAngleDegrees = 25.0; 
+
+    // distance from the center of the Limelight lens to the floor
+    double limelightLensHeightInches = 20.0; 
+
+    // distance from the target to the floor
+    double goalHeightInches = 60.0; 
+
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
+  
+    
+
+    //calculate distance
+    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+
+    //TO DO Convert Distance to Angle
+    this.setpoint = 0; // THIS SHOULD BE that angle
   }
  
   @Override
